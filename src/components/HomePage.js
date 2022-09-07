75637f02cd9365f58af4da7b93f845bfa4dceb55
Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>
2022-09-07 19:03:22 +0530

    modified homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,21 +1,30 @@
-import React from "react";
+import React from 'react';
+import ProductCardStructure from './ProductCardStructure';
+import OrderCardStructure from './OrderCardStructure';
+import { useState } from 'react';
 
 export default function HomePage() {
-  return (
-    <>
+	const [page, setPage] = useState('products');
+	const changePage = () => {
+		setPage(page === 'products' ? 'orders' : 'products');
+	};
 
-      <div className="options">
-        <a href="ProductCardStructure.js/">
-        <div className="feature">
-
-          <h1>My Shop</h1>
-        </div>
-        </a>
-        <div className="feature">
-          <h1>My Order</h1>
-        </div>
-      </div>
-
-    </>
-  );
+	return (
+		<>
+			<div className="options">
+				<button onClick={changePage}>
+					<div className="feature">
+						<h1>My Shop</h1>
+					</div>
+				</button>
+				<button onClick={changePage}>
+					<div className="feature">
+						<h1>My Order</h1>
+					</div>
+				</button>
+			</div>
+			{page === 'products' && <ProductCardStructure />}
+			{page === 'orders' && <OrderCardStructure />}
+		</>
+	);
 }
---
+++ src/pages/ProductPage.js
@@ -0,0 +1,11 @@
+import ProductCardStructure from "../components/productCardStructure";
+
+function Product() {
+    return (
+      <>
+        <ProductCardStructure/>
+      </>
+    );
+  }
+  
+  export default Product;
